a8a116e6fd98bd8ff80e6531cd11c0574c8a02c7
grcjariten <60789009+grcjariten@users.noreply.github.com>
2022-07-25 03:10:06 +0200

    -Creato il Form con la relativa key e trasformati i TextField in TextFormField. -Ho cambiato il TextField di Activity in un Dropdownformfield -Ho intenzione di creare TextFormField separati così da validarli per bene in maniera indipendente (es. ActivityBox, AgeBox, ecc.)
    COSA FARE ANCORA:
    -Fare funzionare il DropdownButtonFormfield per bene
    -Replicarlo anche per i GOALS
    -Validare bene tutto (vd.sopra)
    -Ragiona sulle equazioni e non scordare il discorso Metric/Imperial, se no è un casino dopo

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "TDEE Calculator",
       home: MainPage(),
     );
@@ -22,79 +23,88 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.blueGrey[300],
         appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
+          backgroundColor: Colors.blueGrey[700],
           toolbarHeight: 80,
-          title: const Text("KILOkallo"),
+          title: const Text("TDEE Calculator"),
           centerTitle: true,
           actions: [
             metricImperial(),
               ],
             ),
         body: Padding(
-          padding: const EdgeInsets.fromLTRB(19,17,16,19),
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey[200],
-              borderRadius: const BorderRadius.all(Radius.circular(6))
-            ),
+            padding: const EdgeInsets.fromLTRB(17,12,17,0),
+            child: SingleChildScrollView(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                  borderRadius: const BorderRadius.all(Radius.circular(6))
+                ),
 
-            height: 500, //TODO: Mettere altezza e larghezza in relazione allo schermo
-            width: 400,
-            child:
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(18,10,0,0),
-                  child:
-                      Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              fieldText("Gender",18),
-                              fieldText("Age" , 10),
-                              fieldText("Weight" , 32),
-                              fieldText("Height" , 10),
-                              fieldText("Activity" , 31),
-                              fieldText("BodyFat\n(Opt.)" , 7),
-                              fieldText("Goal" , 26),
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 13),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                height: 540, //TODO: Mettere altezza e larghezza in relazione allo schermo
+                width: 400,
+                child:
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(18,12,0,5),
+                      child:
+                          Form(
+                            key: formKey,
+                            child: Row(
                               children: [
-                                Row(
-                                  children: const [
-                                    Switch(value: false, onChanged: null),
-                                    Text("Man")
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    fieldText("Gender",17),
+                                    fieldText("Age" , 13),
+                                    fieldText("Weight" , 29),
+                                    fieldText("Height" , 14),
+                                    fieldText("Activity" , 25),
+                                    fieldText("BodyFat\n(Opt.)" , 12),
+                                    fieldText("Goal" , 33),
                                   ],
                                 ),
-                                fieldBox(60, "", null),
-                                fieldBox(60, "Kg", null),
-                                fieldBox(60, "cm", null),
-                                fieldBox(150, "Sedentary", null),
-                                fieldBox(60, "15%", null),
-                                goals(),
-                                calculateButton()
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 13),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: const [
+                                          Switch(value: false, onChanged: null),
+                                          Text("Man")
+                                        ],
+                                      ),
+                                      fieldBox(60, "", null, false),
+                                      fieldBox(60, "Kg", null, false),
+                                      fieldBox(60, "cm", null, false),
+                                      activityBox(),
+                                      fieldBox(60, "15%", null, true),
+                                      goals(),
+                                      calculateButton(formKey)
+                                    ],
+                                  ),
+                                ),
                               ],
                             ),
                           ),
-                        ],
-                      ),
 
 
-                ),
+                    ),
 
 
 
 
+              ),
+            ),
           ),
-        ),
+
       bottomNavigationBar: Container(
-        height: 80,
+        height: 70,
         color: Colors.blueGrey,
         child: const Center(child: Text("Banner Admob")),
       ),
@@ -121,14 +131,49 @@ class MainPage extends StatelessWidget {
     );
   }
 
-  Padding fieldBox(double width, String labelText, function) {
+  Padding ageBox(double width, String labelText, function) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: SizedBox(
+        width: width,
+        height: 43,
+        child: TextFormField(
+
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+                return 'Pleaseeeeeeeeeeeeeee';
+            }
+            return null;
+          },
+          obscureText: false,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: labelText,
+          ),
+        ),
+
+      ),
+
+    );
+  }
+
+  Padding fieldBox(double width, String labelText, function, bool optional) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15),
       child: SizedBox(
               width: width,
               height: 43,
-              child: TextField(
-                obscureText: true,
+              child: TextFormField(
+
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    if(optional==false) {
+                      return '* Required field';
+                    }
+                  }
+                  return null;
+                },
+                obscureText: false,
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(),
                   labelText: labelText,
@@ -140,18 +185,55 @@ class MainPage extends StatelessWidget {
     );
   }
 
-  Padding calculateButton() {
+  Padding calculateButton(GlobalKey<FormState> key) {
     return Padding(
       padding: const EdgeInsets.only(top: 15),
       child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-            primary: Colors.blueGrey
+            primary: Colors.blueGrey[700]
           ),
-          onPressed: () {}, child: const Text("Calculate")),
+          onPressed: () {
+            if(key.currentState!.validate()) {
+              print("CIAO!");
+            }
+
+          }, child: const Text("Calculate")),
     );
 
   }
 
+   Padding activityBox() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: SizedBox(
+        width: 170,
+        child: DropdownButtonFormField(
+            value: 0,
+            items: const <DropdownMenuItem> [
+          DropdownMenuItem(
+              value: 0,
+              child: Text("Sedentary")),
+          DropdownMenuItem(
+              value: 1,
+              child: Text("Light Activity")),
+          DropdownMenuItem(
+              value: 2,
+              child: Text("Moderate Activity")),
+          DropdownMenuItem(
+              value: 3,
+              child: Text("High Activity")),
+          DropdownMenuItem(
+              value: 4,
+              child: Text("Elite")),
+        ]
+            , onChanged: null
+        ),
+      ),
+    );
+  }
+  
+  
+  
   ToggleButtons goals() {
 
     Padding choice(String text, IconData iconData, double left, double top, double right, double bottom) {
@@ -172,7 +254,7 @@ class MainPage extends StatelessWidget {
       borderColor: Colors.black,
       isSelected: const [true, true, true],
       children: [
-            choice("Maintain", Icons.accessibility, 20,10,10,10),
+            choice("Maintain", Icons.accessibility, 20,10,10,10), //TODO: Rendili proporzionali alla larghezza schermo
             choice(" Lose ", Icons.arrow_downward, 18,10,18,10),
             choice(" Gain  ", Icons.arrow_upward, 10,10,25,10)
           ],
